fc1a428416dd8f7253a6cf7ac137f9c4898cdbf9
Rogger <rvasque3@gmail.com>
2021-01-03 21:43:50 -0500

    First commit and initial base HTML, no component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { ReactComponent as Logo } from './assets/aerolab-logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className='head'>
+        <Logo title='Store Logo' className='logo' />
+        <div className='user'>
+          <p>John Kite</p>
+          <p>6700 coins</p>
+        </div>
       </header>
-    </div>
-  );
+      <section className='hero'>
+        <h1>Electronics</h1>
+      </section>
+      <section className='container'>
+        <hgroup>
+          <ul className='filter-area'>
+            <li>16 of 32 Products</li>
+            <li className='filter-divider'></li>
+            <li>Sort by:</li>
+            <li>
+              <a href='/' role='button'>
+                Most Recent
+              </a>
+            </li>
+            <li>
+              <a href='/' role='button'>
+                Lowest Price
+              </a>
+            </li>
+            <li>
+              <a href='/' role='button'>
+                Highest Price
+              </a>
+            </li>
+          </ul>
+        </hgroup>
+        <p>Cards</p>
+        <nav>
+          <p>Element 6/12</p>
+          <p>Paginación</p>
+        </nav>
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
