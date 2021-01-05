a62d7c944d3778d858d25e66df281e71cfd2e58c
Rogger <rvasque3@gmail.com>
2021-01-05 08:56:38 -0500

    Initial HTML and CSS Mockup Ready

--- src/App.js
+++ src/App.js
@@ -16,32 +16,56 @@ function App() {
       </section>
       <section className='container'>
         <hgroup>
+          <div className='filter-grid'>
+            <ul className='filter-area'>
+              <li>16 of 32 Products</li>
+              <li className='filter-divider'></li>
+              <li>Sort by:</li>
+              <li>
+                <a href='/' role='button'>
+                  Most Recent
+                </a>
+              </li>
+              <li>
+                <a href='/' role='button'>
+                  Lowest Price
+                </a>
+              </li>
+              <li>
+                <a href='/' role='button'>
+                  Highest Price
+                </a>
+              </li>
+            </ul>
+            <button type='button' className='btn page-changer'></button>
+          </div>
+        </hgroup>
+        <div className='horizontal-divider'></div>
+        <article className='card-grid'>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+          <div className='card'>hola</div>
+        </article>
+        <div className='foot filter-grid'>
           <ul className='filter-area'>
             <li>16 of 32 Products</li>
-            <li className='filter-divider'></li>
-            <li>Sort by:</li>
-            <li>
-              <a href='/' role='button'>
-                Most Recent
-              </a>
-            </li>
-            <li>
-              <a href='/' role='button'>
-                Lowest Price
-              </a>
-            </li>
-            <li>
-              <a href='/' role='button'>
-                Highest Price
-              </a>
-            </li>
           </ul>
-        </hgroup>
-        <p>Cards</p>
-        <nav>
-          <p>Element 6/12</p>
-          <p>Paginación</p>
-        </nav>
+          <button type='button' className='btn page-changer'></button>
+        </div>
+        <div className='horizontal-divider'></div>
       </section>
     </>
   )
