c0ef615528dfd2946b261234f3d0422494b1b5c4
Rogger <rvasque3@gmail.com>
2021-01-05 16:50:01 -0500

    TopHeader fetching data from API

--- src/components/TopHeader/TopHeader.jsx
+++ src/components/TopHeader/TopHeader.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { ReactComponent as Logo } from '../../assets/aerolab-logo.svg'
+import coinIcon from '../../assets/icons/coin.svg'
 import './TopHeader.css'
+import { getUserInfoURL, headers } from '../../utils/apiUtils.js'
 
 function TopHeader() {
+  // TODO: Move Data Fetching to a custom hook
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch(getUserInfoURL, headers)
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(e => console.error(e))
+  }, [])
+
   return (
     <header className='head'>
       <Logo title='Store Logo' className='logo' />
       <div className='user'>
-        <p>John Kite</p>
-        <p>6700 coins</p>
+        <p>{data.name}</p>
+        <div className='point-info'>
+          <p>{data.points}</p>
+          <img src={coinIcon} alt='coins' />
+        </div>
       </div>
     </header>
   )
---
+++ src/utils/apiUtils.js
@@ -0,0 +1,9 @@
+export const headers = {
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmY0YzkwOWIxYjllMDAwMjA3OWJjYTUiLCJpYXQiOjE2MDk4Nzc3Njl9.6_JkzqPxY03pJpMbAxbSn16_-zRvQNrEM9Izo5qviXo'
+  }
+}
+
+export const getUserInfoURL = 'https://coding-challenge-api.aerolab.co/user/me'
