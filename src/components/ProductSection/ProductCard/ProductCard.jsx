ff3aca1d920567c3574d8cc228b1068c5eb3a8fe
Rogger <rvasque3@gmail.com>
2021-01-07 17:53:58 -0500

    Hover behavior added for cards

--- src/components/ProductSection/ProductCard/ProductCard.jsx
+++ src/components/ProductSection/ProductCard/ProductCard.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProductCard.css'
 import { ReactComponent as RedeemButton } from '../../../assets/icons/buy-blue.svg'
+import { ReactComponent as WhiteRedeemButton } from '../../../assets/icons/buy-white.svg'
+import { ReactComponent as CoinIcon } from '../../../assets/icons/coin.svg'
 
-function ProductCard({ img, name, category }) {
+function ProductCard({ img, name, category, cost }) {
+  const [isHovered, setIsHovered] = useState(false)
+
+  const toggleHover = status => {
+    setIsHovered(status)
+  }
+
   return (
-    <div className='card'>
+    <div
+      className='card'
+      onMouseEnter={() => toggleHover(true)}
+      onMouseLeave={() => toggleHover(false)}>
       <div className='card-image'>
         <img src={img.url} alt='dell x1' />
-        <button className='redeem-button'>
-          <RedeemButton />
+        <button className='buy-button'>
+          {isHovered ? <WhiteRedeemButton /> : <RedeemButton />}
         </button>
       </div>
       <div className='small-horizontal-divider'></div>
@@ -16,6 +27,17 @@ function ProductCard({ img, name, category }) {
         <p className='product-category'>{category}</p>
         <p className='product-title'>{name}</p>
       </div>
+      {isHovered ? (
+        <div className='redeem-panel'>
+          <div className='redeem-price'>
+            <p>{cost}</p>
+            <CoinIcon />
+          </div>
+          <button className='redeem-button'>Redeem Now</button>
+        </div>
+      ) : (
+        ''
+      )}
     </div>
   )
 }
